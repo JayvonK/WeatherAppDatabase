053141041851d02959bef8cc4d3a1994fa156bd4
JayvonK <jaykhuth@gmail.com>
2024-01-13 14:40:45 -0800

    clean up

--- Controllers/FavoriteCitiesController.cs
+++ Controllers/FavoriteCitiesController.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+namespace WeatherAppDatabase.Controllers;
 
-namespace WeatherAppDatabase.Controllers
-{
     [ApiController]
     [Route("[controller]")]
     public class FavoriteCitiesController : ControllerBase
     {
         
     }
-}
