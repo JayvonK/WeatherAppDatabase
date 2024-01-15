19a1a9f5789266f2521c4c84b1b0ef5729d3bdb7
JayvonK <jaykhuth@gmail.com>
2024-01-14 21:09:33 -0800

    adding get

--- Services/IFavoritesService.cs
+++ Services/IFavoritesService.cs
@@ -4,6 +4,8 @@ namespace WeatherAppDatabase.Services;
 
     public interface IFavoritesService
     {
+        List<Favorites> GetFavorites();
+
         string AddFavCity(string city);
 
         List<Favorites> DeleteFavCity(string city);
